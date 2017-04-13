08e5364910e9adb38be9f34aaa0f3dd1dae1333f
Dimitris Niras <niras.94@gmail.com>
2017-04-13 12:01:15 +0300

    Add files via upload

--- background.js
+++ background.js
@@ -1,2 +1,43 @@
-//background.js
+var active = false;
+var empty = true;
+var screen = "start";
+var test_seq = new Array();
 
+chrome.runtime.onMessage.addListener(function(req, send, sendResponse) {
+	console.log(req);
+	if (req.action == "get_status") {
+		sendResponse({'active': active, 'empty': empty, 'screen': screen});
+	}
+	
+	if (req.action == "start") {
+		if (!active) {
+			active = true;
+			empty = true;
+			test_seq = new Array();
+			sendResponse({start: true});
+		}
+	}
+	
+	if (req.action == "stop") {
+		active = false;
+		screen = "stop";
+		sendResponse({});
+	}
+	
+	if (req.action == "done") {
+		active = false;
+		screen = "done";
+		sendResponse({});
+	}
+	
+	if (req.action == "save") {
+		active = false;
+		screen = "save";
+		sendResponse({});
+	}
+	
+	if (req.action == "get_array") {
+		sendResponse({'array': test_seq});
+	}
+	
+});
--- popup.js
+++ popup.js
@@ -1,21 +1,3 @@
-//popup.js
-
-var startButton = document.getElementById("btn-start");
-var loginButton = document.getElementById("btn-login");
-var signUpButton = document.getElementById("btn-signup");
-var errorLoginButton = document.getElementById("btn-error-login");
-var doneButton = document.getElementById("btn-done");
-var cancelRecordButton = document.getElementById("btn-cancel-record");
-var saveButton = document.getElementById("btn-save");
-var cancelSaveButton = document.getElementById("btn-cancel-save");
-var resultButton = document.getElementById("btn-result");
-
-startButton.addEventListener("click", startRecording);
-doneButton.addEventListener("click", doneRecording);
-cancelRecordButton.addEventListener("click", cancelRecording);
-cancelSaveButton.addEventListener("click", cancelSaving);
-saveButton.addEventListener("click", saveRecord);
-
 function appear(screen) {
 	return document.querySelector(screen).style.display = "block";
 }
@@ -24,28 +6,129 @@ function dissapear(screen) {
 	return document.querySelector(screen).style.display = "none";
 }
 
-function startRecording() {
+function recorderProxy() {
+	this.active = null;
+}
+
+recorderProxy.prototype.start = function() {
+	chrome.runtime.sendMessage({action: "start"});
+}
+
+recorderProxy.prototype.done = function() {
+	chrome.runtime.sendMessage({action: "done"});
+}
+
+recorderProxy.prototype.stop = function() {
+	chrome.runtime.sendMessage({action: "stop"});
+}
+
+recorderProxy.prototype.saving = function() {
+	chrome.runtime.sendMessage({action: "save"});
+}
+
+function recorderUI() {
+	this.recorder = new recorderProxy();
+	
+	chrome.runtime.sendMessage({action: "get_status"}, function(response) {
+		console.log(response.active);
+		if (response.active) {
+			ui.startRecording();
+		}
+		else {
+			if (response.screen == "start") ui.setStarted();
+			else if (response.screen == "done") ui.doneRecording();
+			else if (response.screen == "stop") ui.setStarted();
+			else if (response.screen == "save") ui.saveRecord();
+		}
+	});
+}
+
+recorderUI.prototype.setStarted = function() {
+	dissapear("#scr-recording");
+	dissapear("#scr-save");
+	dissapear("#scr-result");
+	appear("#scr-start");
+}
+
+recorderUI.prototype.startRecording = function() {
 	dissapear("#scr-start");
 	appear("#scr-recording");
+	
+	ui.recorder.start();
+	
+	return false;
 }
 
-function doneRecording() {
+recorderUI.prototype.doneRecording = function() {
 	dissapear("#scr-recording");
 	appear("#scr-save");
+	
+	ui.recorder.done();
+	
+	return false;
 }
 
-function cancelRecording() {
+recorderUI.prototype.cancelRecording = function() {
 	dissapear("#scr-recording");
 	appear("#scr-start");
+	
+	ui.recorder.stop();
+	
+	return false;
 }
 
-function cancelSaving() {
-	dissapear("#scr-save");
-	appear("#scr-start");
-}
-
-function saveRecord() {
+recorderUI.prototype.saveRecord = function() {
 	dissapear("#scr-save");
 	appear("#scr-result");
+	
+	ui.recorder.saving();
+	
+	return false;
+}
+
+recorderUI.prototype.cancelSaving = function() {
+	dissapear("#scr-save");
+	appear("#scr-start");
+	
+	ui.recorder.stop();
+	
+	return false;
+}
+
+var ui;
+
+window.onload = function() {
+	document.querySelector('button#btn-start').onclick = function() {
+		ui.startRecording();
+		return false;
+	};
+	
+	document.querySelector('button#btn-done').onclick = function() {
+		ui.doneRecording();
+		return false;
+	};
+	
+	document.querySelector('button#btn-cancel-record').onclick = function() {
+		ui.cancelRecording();
+		return false;
+	};
+	
+	document.querySelector('button#btn-save').onclick = function() {
+		ui.saveRecord();
+		return false;
+	};
+	
+	document.querySelector('button#btn-cancel-save').onclick = function() {
+		ui.cancelSaving();
+		return false;
+	};
+	
+	document.querySelector('a#btn-result').onclick = function() {
+		ui.setStarted();
+		ui.recorder.stop();
+		return false;
+	};
+	
+	ui = new recorderUI();
 }
 
