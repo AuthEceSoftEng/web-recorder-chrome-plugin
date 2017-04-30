c1ae267fc4cff4a9ee36713dab3929b57f5d5703
Dimitris Niras <niras.94@gmail.com>
2017-04-30 13:44:24 +0300

    Add files via upload

--- content.js
+++ content.js
@@ -72,15 +72,6 @@ function startRecorder() {
 		var val = identify(this);
 		
 		sendMsg("change", val[0], val[1], this.value);
-		
-		/*$(document).keypress(function() {
-			if (event.which == 13) {
-				var val = identify(event.target);
-			
-				sendMsg("enterKey", val[0], val[1], "");
-			}
-		});*/
-	
 	});
 
 	selects.click(function() {
@@ -120,6 +111,10 @@ function identify(e) {
 		identifier = e.name;
 		type = "name";
 	}
+	else if (cssSelector(e) != "") {
+		identifier = cssSelector(e).replace(/\\\"/g, '\\\\\\"');
+		type = "css";
+	}
 	else if (e.className != "") {
 		identifier = e.className;
 		type = "className";
@@ -128,12 +123,98 @@ function identify(e) {
 		identifier = e.text;
 		type = "linkText";
 	}
+	else if (getXPath(e) != "") {
+		identifier = getXPath(e);
+		type = "xpath";
 	
 	return [type, identifier];
 }
 
 function sendMsg(type, identifier, id_value, input_value) {
 	chrome.runtime.sendMessage({action: "append", obj: {type: type, identifier: identifier, id: id_value, input: input_value}});
+}
+
+var cssSelector = function (target) {
+	var query = '';
+
+    if (target == document)
+    	query = 'body';
+    else {
+    	var attr = ['ng-model', 'ng-href', 'name', 'aria-label'].reduce(function (a, b) { return a || (target.getAttribute(b) ? b : null); }, null);
+    	
+        if (attr)
+        	query = target.tagName.toLowerCase() + '[' + attr + '="' + target.getAttribute(attr).replace(/\\/g, '\\\\').replace(/\'/g, '\\\'').replace(/\"/g, '\\"').replace(/\0/g, '\\0') + '"]';
+        else
+        	query = target.tagName.toLowerCase();
+
+		var nodes = target.parentNode.querySelectorAll(query);
+		
+        if (nodes && nodes.length > 1)
+        	query += ':nth-of-type(' + (Array.prototype.slice.call(nodes).indexOf(target) + 1).toString() + ')';
+
+		query = query.replace(/\s/g, ' ');
+	}
+
+    if (document.querySelectorAll(query).length > 1 && target.parentNode)
+    	query = cssSelector(target.parentNode) + '>' + query;
+
+    return query;
+    
+};
+
+function getXPath(node) {
+    var comp, comps = [];
+    var parent = null;
+    var xpath = '';
+    var getPos = function(node) {
+        var position = 1, curNode;
+        if (node.nodeType == Node.ATTRIBUTE_NODE) {
+            return null;
+        }
+        for (curNode = node.previousSibling; curNode; curNode = curNode.previousSibling) {
+            if (curNode.nodeName == node.nodeName) {
+                ++position;
+            }
+        }
+        return position;
+     }
+
+    if (node instanceof Document) {
+        return '/';
+    }
+
+    for (; node && !(node instanceof Document); node = node.nodeType == Node.ATTRIBUTE_NODE ? node.ownerElement : node.parentNode) {
+        comp = comps[comps.length] = {};
+        switch (node.nodeType) {
+            case Node.TEXT_NODE:
+                comp.name = 'text()';
+                break;
+            case Node.ATTRIBUTE_NODE:
+                comp.name = '@' + node.nodeName;
+                break;
+            case Node.PROCESSING_INSTRUCTION_NODE:
+                comp.name = 'processing-instruction()';
+                break;
+            case Node.COMMENT_NODE:
+                comp.name = 'comment()';
+                break;
+            case Node.ELEMENT_NODE:
+                comp.name = node.nodeName;
+                break;
+        }
+        comp.position = getPos(node);
+    }
+
+    for (var i = comps.length - 1; i >= 0; i--) {
+        comp = comps[i];
+        xpath += '/' + comp.name;
+        if (comp.position != null) {
+            xpath += '[' + comp.position + ']';
+        }
+    }
+
+    return xpath;
+
 }
 
 chrome.runtime.onMessage.addListener(function(req, send, sendResponse) {
@@ -165,4 +246,7 @@ setInterval(function() {
 		}
     });
 }, 500);
+
+
+
 
